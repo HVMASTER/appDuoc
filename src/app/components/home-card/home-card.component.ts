a8b29db9f5916dc24701a85e3c9a41975b378e78
Hernán <he.vallejos@duocuc.cl>
2023-10-01 20:48:11 -0300

    creacion modal

--- src/app/components/home-card/home-card.component.ts
+++ src/app/components/home-card/home-card.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { IonModal } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { RegistroService } from 'src/app/modules/auth/auth.service';
 import { Router } from '@angular/router';
+import { OverlayEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-home-card',
@@ -13,7 +15,29 @@ import { Router } from '@angular/router';
 })
 export class HomeCardComponent  implements OnInit {
 
+  message = '';
+
+  @ViewChild(IonModal) modal!: IonModal;
+
   constructor(private registroService: RegistroService, private router: Router) { }
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+    console.log('cancel');
+    this.router.navigate(['/home']);
+  }
+
+  confirm() {
+    this.modal.dismiss(null, 'confirm');
+    console.log('confirm');
+  }
+
+  onWillDismiss(event: Event){
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm'){
+      this.message = `hola, ${ev.detail.data}!`;
+    };
+  }
 
   ngOnInit() {
 
