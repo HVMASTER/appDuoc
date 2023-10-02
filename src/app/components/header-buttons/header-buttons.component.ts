34366820aa9febff1506dce3cbb5643d1f7dc12c
Hernán <he.vallejos@duocuc.cl>
2023-10-01 22:55:07 -0300

    se incorpora registro de conductor en el localstorage

--- src/app/components/header-buttons/header-buttons.component.ts
+++ src/app/components/header-buttons/header-buttons.component.ts
@@ -14,15 +14,20 @@ export class HeaderButtonsComponent  implements OnInit {
 
   nombre= '';
   apellidos= '';
+  tipo= '';
 
   constructor(private registroService: RegistroService) {  }
 
   //Este es el método ngOnInit que se ejecutará cuando el componente se inicialice. Aquí se obtiene el usuario logueado desde el servicio y se asignan sus valores al nombre y apellidos del componente, si existe un usuario logueado.
   ngOnInit() {
     const usuarioLogueado = this.registroService.usuarioLogueado;
+    const conductorLogueado = this.registroService.conductorLogueado;
     if (usuarioLogueado) {
       this.nombre = usuarioLogueado.nombre;
       this.apellidos = usuarioLogueado.apellidos
+      if (conductorLogueado) {
+        this.tipo = conductorLogueado.tipo;
+      }
     }
   }
 }
