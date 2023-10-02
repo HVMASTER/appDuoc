84ac2760b922f8d91edd50eafba15275cdd0db82
Hernán <he.vallejos@duocuc.cl>
2023-10-01 21:59:14 -0300

    se agrega modal a solicitud viaje

--- src/app/components/home-card/home-card.component.ts
+++ src/app/components/home-card/home-card.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { RegistroService } from 'src/app/modules/auth/auth.service';
 import { Router } from '@angular/router';
 import { OverlayEventDetail } from '@ionic/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home-card',
@@ -18,17 +19,18 @@ export class HomeCardComponent  implements OnInit {
   message = '';
 
   @ViewChild(IonModal) modal!: IonModal;
+  modalController: any;
 
-  constructor(private registroService: RegistroService, private router: Router) { }
+  constructor(private registroService: RegistroService, private router: Router, modalController: ModalController) { }
 
   cancel() {
-    this.modal.dismiss(null, 'cancel');
+    this.modal.dismiss({ confirmed: true});
     console.log('cancel');
     this.router.navigate(['/home']);
   }
 
   confirm() {
-    this.modal.dismiss(null, 'confirm');
+    this.modal.dismiss({ confirmed: false});
     console.log('confirm');
   }
 
@@ -43,8 +45,35 @@ export class HomeCardComponent  implements OnInit {
 
   }
 
-  functionFrom() {
-    this.router.navigate(['/login']);
+  functionFrom(){
+    this.router.navigate(['/home']);
   }
 
+  registroConductor(){
+    this.router.navigate(['/registro-conductor']);
+  }
+
+  abrirModal() {
+    this.presentModal();
+  }
+
+  async presentModal() {
+
+    const modal = await this.modalController.create({
+      component: 'app-home-card',
+      componentProps: {
+        custom_id: this.registroService.modalPrueba()
+      }
+    });
+    await modal.present();
+
+    const { data } = await modal.onWillDismiss();
+    if (data) {
+      // Se ejecutará si se confirma o cancela el modal, data contendrá la información que necesites
+      console.log('Modal cerrado. Datos recibidos:', data);
+
+      // Redireccionar a la página de inicio
+      this.router.navigate(['/home']);
+    }
+  }
 }
