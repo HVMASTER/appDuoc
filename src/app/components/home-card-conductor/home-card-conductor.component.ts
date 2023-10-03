0ac6af80b96da96f2027b0acd5441bf9d5a39e6c
OSCARS26 <oscarolivare1@gmail.com>
2023-10-03 01:11:34 -0300

    Creación componente conductor

---
+++ src/app/components/home-card-conductor/home-card-conductor.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-card-conductor',
+  templateUrl: './home-card-conductor.component.html',
+  styleUrls: ['./home-card-conductor.component.scss'],
+})
+export class HomeCardConductorComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
