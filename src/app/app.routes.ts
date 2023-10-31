65cc094c65d966470ab3cbbcfb73ed8803bc00d2
Hernán <he.vallejos@duocuc.cl>
2023-10-30 21:48:46 -0300

    se implementan guards a las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { HomePage } from './modules/home/home.page';
 import { HomeConductorPage } from './modules/home-conductor/home-conductor.page';
+import { AuthGuard } from './guard/authUser.guard';
+import { AuthGuardConductor } from './guard/auth-conductor.guard';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./modules/home/home.page').then((m) => m.HomePage),
-  },
   {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full',
   },
+  {
+    path: 'home',
+    canActivate: [AuthGuard],
+    loadComponent: () => import('./modules/home/home.page').then((m) => m.HomePage),
+  }
+  ,
   {
     path: 'registro',
     loadComponent: () => import('./modules/auth/registro/registro.page').then( m => m.RegistroPage)
@@ -26,10 +31,12 @@ export const routes: Routes = [
   },
   {
     path: 'registro-conductor',
+    canActivate: [AuthGuard],
     loadComponent: () => import('./modules/auth/registro-conductor/registro-conductor.page').then( m => m.RegistroConductorPage)
   },
   {
     path: 'home-conductor',
+    canActivate: [AuthGuardConductor],
     loadComponent: () => import('./modules/home-conductor/home-conductor.page').then( m => m.HomeConductorPage)
   },
   {
@@ -38,10 +45,12 @@ export const routes: Routes = [
   },
   {
     path: 'solicitud',
+    canActivate: [AuthGuard],
     loadComponent: () => import('./modules/solicitud/solicitud.page').then( m => m.SolicitudPage)
   },
   {
     path: 'crear-viaje',
+    canActivate: [AuthGuardConductor],
     loadComponent: () => import('./modules/crear_viaje/viajes.page').then( m => m.ViajesPage)
   },
   //Rutas del Footer
